139372197d824463a4f8d0d3a7be1e8e232431e6
Martynas Kan <martis347@gmail.com>
2014-12-17 23:43:37 +0200

    Beggining

---
+++ Service/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+using Warrior;
+using log4net;
+using log4net.Config;
+
+namespace Service
+{
+    public class Program
+    {
+        private static ILog Log;
+        static void Main(string[] args)
+        {
+            Log = LogManager.GetLogger("InfrastructureLogger");
+            XmlConfigurator.Configure(new FileInfo("..\\..\\App.config"));
+            var restartDelay = (int)TimeSpan.FromMinutes(1).TotalMinutes;
+
+            HostFactory.Run(config =>
+            {
+                config.Service<Business>(svc =>
+                {
+                    svc.ConstructUsing(s => new Business());
+                    svc.WhenStarted(s => s.Start());
+                    svc.WhenStopped(s => s.Stop());
+                });
+                Log.Info("Program is working");
+                config.SetServiceName("MyService");
+                config.SetDisplayName("My service");
+                config.SetDescription("My service via Topshelf");
+                config.RunAsLocalService();
+                config.DependsOnEventLog();
+                config.StartAutomatically();
+                config.EnableServiceRecovery(recovery => recovery.RestartService(restartDelay));
+            });
+        }
+    }
+}
---
+++ Warrior/Business.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using log4net;
+using log4net.Config;
+
+namespace Warrior
+{
+    public class Business
+    {
+        private static ILog Log;
+        public Business()
+        {
+            Log = LogManager.GetLogger("BusinessLogger");
+            XmlConfigurator.Configure(new FileInfo("..\\..\\App.config"));
+        }
+
+        
+        private bool write = true;
+        public void Start()
+        {
+            Log.Info("Program start!");
+            while (write)
+            {
+                Console.WriteLine("They see me runnin', they hatin'");
+                Thread.Sleep(1000);         
+            }
+        }
+        public void Stop()
+        {
+            Log.Info("Program stop!");
+            write = false;
+        }
+    }
+}
